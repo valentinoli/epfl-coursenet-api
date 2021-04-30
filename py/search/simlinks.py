582485fba9514401dcd5621aa7df21572edc0a61
valentinoli <valentin.loftsson@gmail.com>
2021-04-30 22:15:15 +0200

    Add simlinks.py

---
+++ py/search/simlinks.py
@@ -0,0 +1,34 @@
+import sys
+
+import pandas as pd
+from numpy import nan
+from search import helpers
+
+# threshold_parameter = int(float(sys.argv[1]) * 100)
+# slugs = eval(sys.argv[2])
+
+stdin1, stdin2 = sys.stdin.readline().split('#')
+
+similarity_threshold = int(float(stdin1) * 100)
+slugs = stdin2.split(',')
+
+index = helpers.load_df('index')
+
+df = (
+    pd.DataFrame.sparse.from_spmatrix(
+        helpers.load_sparsemat(f'sim{similarity_threshold}'),
+        index=index.rename('source'),
+        columns=index.rename('target')
+    )
+    .astype(pd.SparseDtype('float', nan))
+)
+
+# Filter out courses which don't match provided slugs
+boolean_index = index.isin(slugs).values
+df = df.loc[boolean_index, boolean_index]
+
+# pair row and column indices (ignores NaNs)
+df = df.stack().sparse.to_dense().rename('similarity').reset_index()
+df['id'] = df.source + '--' + df.target
+
+print(df.to_json(orient='records'))
